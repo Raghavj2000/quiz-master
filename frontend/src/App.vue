773cd0eafd749c6461d24f6c657c330b69c2a59e
Raghavendra J <RaghavendraJ@ziniosedge.com>
2025-03-17 23:04:53 +0530

    Sample

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">hii</div>
+  </header>
+
+  <RouterView />
+</template>
